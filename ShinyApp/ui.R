9019e45f5fad7ab8acc59b6ce9a2910153bf2f36
Pseud-F <jshumdrum123@gmail.com>
2021-09-17 11:29:48 +0100

    theme changes to app ui

--- ShinyApp/ui.R
+++ ShinyApp/ui.R
@@ -10,10 +10,12 @@
 library(shiny)
 library(DT)
 library(plotly)
-
+library(shinythemes)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
+    
+    theme = shinytheme("superhero"),
     
     tags$head(
         
@@ -21,39 +23,67 @@ shinyUI(fluidPage(
         
         tags$style(type="text/css", "#value{ height: 200px; word-wrap: break-word; }")
         
-    ),    
-
-    # Application title
-    titlePanel("Orbital Launch Data App"),
-
+    ),     
     
-    sidebarLayout(
-        sidebarPanel(
-            # Check Box for either LEO or GTO orbital launchers
-            radioButtons("LEO_GTO","Select Payload-capability:",
-                               choices = c("LEO","GTO")),
-            # Check Box for Current/Future and Retired/Cancelled Orbital Launchers
-            checkboxGroupInput("current_retired","Select Operational Status:",
-                               choices = c("Current/Future","Retired/Cancelled"),
-                               selected = "Current/Future"),
-            # Slider input to show Orbital Launchers above a minimum number of launches
-            sliderInput("min_launches",
-                        "Select Minimum Number of Launches:",
-                        min = 0,
-                        max = 500,
-                        value = 5,),
-            # Check box input to show logarithmic or linear scale on plot
-            radioButtons("log_linear","Plot linear or logarithmic scale:",
-                         choices = c("Linear","Log"),selected = "Log")            
-        ),
+    navbarPage(
 
-        # Show a plot of the Orbital Launchers
-        mainPanel(
-            tabsetPanel(type = "tabs",
-                        tabPanel("Payload vs. First Flight", br(),plotlyOutput("launchplot",width=900,height=700)),
-                        tabPanel("Summary by Country",br(),DT::dataTableOutput('summary')),
-                        tabPanel("Documentation", br(), htmlOutput('about')))
-            
-        )
+        "Orbital Launch Data App",
+        ##########
+        ## Page 1
+        ##########        
+        tabPanel(
+            "Home",
+            sidebarLayout(
+                sidebarPanel(
+                    # Check Box for either LEO or GTO orbital launchers
+                    radioButtons("LEO_GTO","Select Payload-capability:",
+                                 choices = c("LEO","GTO")),
+                    # Check Box for Current/Future and Retired/Cancelled Orbital Launchers
+                    checkboxGroupInput("current_retired","Select Operational Status:",
+                                       choices = c("Current/Future","Retired/Cancelled"),
+                                       selected = "Current/Future"),
+                    # Slider input to show Orbital Launchers above a minimum number of launches
+                    sliderInput("min_launches",
+                                "Select Minimum Number of Launches:",
+                                min = 0,
+                                max = 500,
+                                value = 5,),
+                    # Check box input to show logarithmic or linear scale on plot
+                    radioButtons("log_linear","Plot linear or logarithmic scale:",
+                                 choices = c("Linear","Log"),selected = "Log")            
+                ),
+                mainPanel(
+                    
+                    tags$style(HTML("
+                    .dataTables_wrapper .dataTables_length, .dataTables_wrapper .dataTables_filter, .dataTables_wrapper .dataTables_info, .dataTables_wrapper .dataTables_processing, .dataTables_wrapper .dataTables_paginate {
+                    color: #ffffff;
+                    }
+
+                    thead {
+                    color: #ffffff;
+                    }
+
+                     tbody {
+                    color: #000000;
+                    }
+
+                   "
+                                    
+                                    
+                    )),
+                    tabsetPanel(type = "tabs",
+                                tabPanel("Payload vs. First Flight", br(),plotlyOutput("launchplot",width=900,height=700)),
+                                tabPanel("Summary by Country",br(),DT::dataTableOutput('summary'))
+                    )
+                )
+            )
+        ),
+        
+        tabPanel(
+            "Documentation",
+            mainPanel(htmlOutput('about'))
+                
+            )
+        
     )
 ))
